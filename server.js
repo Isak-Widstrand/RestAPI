0cf2651db927469e6c9120bd607edadfab985a03
Isak-Widstrand <90836489+Isak-Widstrand@users.noreply.github.com>
2023-05-14 19:05:47 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -25,10 +25,6 @@ app.use(bodyParser.json())
 
 const crypto = require('crypto'); //använder NodeJS inbyggda krypteringsfunktioner.
 
-/*
- Funktion som tar någon form av indata, t.ex. ett lösenord i klartext,
- hashar det och returnerar hashvärdet som en sträng.
-*/
 function hash(data) {
  const hash = crypto.createHash('sha256');
  hash.update(data);
@@ -69,11 +65,12 @@ app.get('/users/:Id', function(req, res) {
 
 app.post('/users', function(req, res) {
   let sql = `Select * FROM users where Name = '${req.body.Name}'`;
+  const Password_hash = hash(password)
   connection.query(sql, function (err, results) {
     if (err) throw err;
     if (results.length === 0){
-      let sql = `INSERT INTO users(Name, Username, Password)
-      VALUES ('${req.body.Name}', '${req.body.Username}', '${req.body.Password}')`
+      let sql = `INSERT INTO users(Name, Username, Password_hash)
+      VALUES ('${req.body.Name}', '${req.body.Username}', '${req.body.Password_hash}')`
       connection.query(sql, function(err, result) {
         if (err) throw err;
         //kod här för att hantera returnera data…
@@ -104,6 +101,28 @@ app.put('/users/:id', (req, res) => {
       res.status(400).send('User not found.');
     } else {
       res.status(200).send('User updated successfully.');
+    }
+  });
+});
+
+app.post('/login', function(req, res) {
+  let Username = req.body.Username;
+  let Password = req.body.Password;
+  let sql = `SELECT * FROM users WHERE username = '${Username}'`;
+
+  connection.query(sql, function (err, results, fields) {
+    if (err) throw err;
+
+    if (results.length === 0) {
+      res.status(401).json({ message: "Invalid username or password" });
+    } else {
+      let user = results[0];
+      
+      if (user.Password_hash === hash(Password)) {
+        res.json({ message: "Login successful" });
+      } else {
+        res.status(401).json({ message: "Invalid username or password" });
+      }
     }
   });
 });
